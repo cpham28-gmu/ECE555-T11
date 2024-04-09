1b0503994409d01f6e15db0deb92557a12c26199
Kendall Meiners <36490633+Kmeiners@users.noreply.github.com>
2024-04-09 12:19:17 -0400

    Update FFT_CUDA.cpp
    add create and update scaling

--- FFT_CUDA.cpp
+++ FFT_CUDA.cpp
@@ -1,11 +1,38 @@
 #include <complex>
 #include <iostream>
 #include <vector>
+#include <stdbool.h>
 #include <cufft.h>
 
 #include "peaks.h"
 
-#define FFT_SIZE 8
+#define FFT_SIZE 1024
+#define DEFAULT_FILE_NAME "input.csv"
+
+vector<float> input(FFT_SIZE, 0);
+
+int create(string filename)
+{
+    FILE *f = fopen(filename.c_str(), "r");
+
+    if (f == nullptr)
+        return -1;
+
+    char buff[255];
+    char *end;
+
+    int i;
+    for (i = 0; i < FFT_SIZE; i++)
+    {
+        if (fgets(buff, 255, (FILE *)f) == NULL)
+            break;
+        input[i] = strtod(buff, &end);
+        // printf("%lld\n", NumbersO[i]);
+    }
+    fclose(f);
+
+    return i;
+}
 
 __global__
 void kernel(cufftComplex* data, float scale) {
@@ -20,20 +47,35 @@ void kernel(cufftComplex* data, float scale) {
 int main(int argc, char *argv[]) {
     cufftHandle planReal, planComp;
     cudaStream_t stream = NULL;
+    bool debug = false;
 
-    vector<float> input(FFT_SIZE, 0);
+    // vector<float> input(FFT_SIZE, 0);
     vector<float> data_mag_sq(FFT_SIZE/2, 0);
     vector<complex<float>> output(FFT_SIZE / 2 + 1);
 
-    for (auto i = 0; i < FFT_SIZE; i++) {
-        input[i] = i;
+    // Take in file name as arg
+    string filename = DEFAULT_FILE_NAME;
+    if (argc > 1)
+    {
+        debug = argv[1];
     }
 
-    printf("Input array:\n");
-    for (auto &i : input) {
-        printf("%f\n", i);
+    // Pull entries from data points
+    int count = create(filename);
+
+    cout << "Took in " << count << " entries" << endl;
+
+    // for (auto i = 0; i < FFT_SIZE; i++) {
+    //     input[i] = i;
+    // }
+
+    if (debug == 1){
+        printf("Input array:\n");
+        for (auto &i : input) {
+            printf("%f\n", i);
+        }
+        printf("======================\n");
     }
-    printf("======================\n");
 
     float *d_input = nullptr;
     cufftComplex *d_output = nullptr;
@@ -59,28 +101,36 @@ int main(int argc, char *argv[]) {
 
     cudaStreamSynchronize(stream);
 
-    printf("Output after Forward FFT:\n");
-    for (auto &i : output) {
-        printf("%f + %fi\n", i.real(), i.imag());
+    if (debug == 1){
+        printf("Output after Forward FFT:\n");
+        for (auto &i : output) {
+            printf("%f + %fi\n", i.real(), i.imag());
+        }
+        printf("======================\n");
     }
-    printf("======================\n");
 
     // Normalize the data
-    kernel<<<1, 128, 0, stream>>>(d_output, 1.f/FFT_SIZE);
+    kernel<<<1, FFT_SIZE, 0, stream>>>(d_output, 1.f/FFT_SIZE);
+
+    cudaMemcpyAsync(output.data(), d_output, sizeof(complex<float>) * output.size(), cudaMemcpyDeviceToHost, stream);
+
+    cudaStreamSynchronize(stream);
 
     // Magnitude
-    for (int i = 0; i < (FFT_SIZE/2); i++)
+    for (int i = 0; i < (FFT_SIZE/2+1); i++)
     {
-        data_mag_sq[i] = output[i].real() * output[i].real() + output[i].imag() * output[i].imag();
+        data_mag_sq[i] = sqrt(output[i].real() * output[i].real() + output[i].imag() * output[i].imag());
     }
     data_mag_sq[0] /= 2;
     data_mag_sq[(FFT_SIZE/2)-1] /= 2;
 
-    printf("Values with magnitude squared:\n");
-    for (auto i = 0; i < input.size()/2; i++) {
-        printf("%f\n", data_mag_sq[i]);
+    if (debug){
+        printf("Values with magnitude squared:\n");
+        for (auto i = 0; i < data_mag_sq.size(); i++) {
+            printf("%f\n", data_mag_sq[i]);
+        }
+        printf("======================\n");
     }
-    printf("======================\n");
 
     vector<peak> peaks(FFT_SIZE);
     find_peaks(data_mag_sq, peaks);
@@ -90,11 +140,13 @@ int main(int argc, char *argv[]) {
 
     cudaMemcpyAsync(input.data(), d_input, sizeof(float) * input.size(), cudaMemcpyDeviceToHost, stream);
 
-    printf("Values after inverted FFT:\n");
-    for (auto &i : input) {
-        printf("%f\n", i);
+    if (debug == 1){
+        printf("Values after inverted FFT:\n");
+        for (auto &i : input) {
+            printf("%f\n", i);
+        }
+        printf("======================\n");
     }
-    printf("======================\n");
 
     // Free used resources
     cudaFree(d_input);
