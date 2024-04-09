55115a8c587d797eeb736f3071eb70e98575c4e1
Kendall Meiners <36490633+Kmeiners@users.noreply.github.com>
2024-04-09 13:59:00 -0400

    Update FFT_CUDA.cpp
    Add timings and fix some scaling errors

--- FFT_CUDA.cpp
+++ FFT_CUDA.cpp
@@ -6,11 +6,14 @@
 
 #include "peaks.h"
 
-#define FFT_SIZE 1024
+#define FFT_SIZE 100000
 #define DEFAULT_FILE_NAME "input.csv"
 
 vector<float> input(FFT_SIZE, 0);
 
+/*
+    Pull in the results from the included input.csv file 
+*/
 int create(string filename)
 {
     FILE *f = fopen(filename.c_str(), "r");
@@ -34,6 +37,10 @@ int create(string filename)
     return i;
 }
 
+/*
+    CUDA kernel for applying scale to the results of the Forward FFT,
+    Normalizes the results before magnitude and peak analysis
+*/
 __global__
 void kernel(cufftComplex* data, float scale) {
     const int tid = threadIdx.x + blockIdx.x * blockDim.x;
@@ -48,26 +55,27 @@ int main(int argc, char *argv[]) {
     cufftHandle planReal, planComp;
     cudaStream_t stream = NULL;
     bool debug = false;
+    cudaEvent_t time1, time2, time3, time4;
+    float totalTime, ForFFTTime, ScalMagTime, RevFFTTime;
+    int bins = FFT_SIZE/2 + 1;
+    int blockSize = 100;
 
-    // vector<float> input(FFT_SIZE, 0);
-    vector<float> data_mag_sq(FFT_SIZE/2, 0);
-    vector<complex<float>> output(FFT_SIZE / 2 + 1);
+    vector<float> data_mag_sq(bins, 0);
+    vector<complex<float>> output(bins);
 
     // Take in file name as arg
     string filename = DEFAULT_FILE_NAME;
     if (argc > 1)
     {
-        debug = argv[1];
+        int block = atoi(argv[1]);
+        blockSize = block;
+        printf("using %d block(s)\n", blockSize);
     }
 
     // Pull entries from data points
     int count = create(filename);
 
     cout << "Took in " << count << " entries" << endl;
-
-    // for (auto i = 0; i < FFT_SIZE; i++) {
-    //     input[i] = i;
-    // }
 
     if (debug == 1){
         printf("Input array:\n");
@@ -94,12 +102,20 @@ int main(int argc, char *argv[]) {
     cudaMalloc((void **)&d_output, sizeof(complex<float>) * output.size());
     cudaMemcpyAsync(d_input, input.data(), sizeof(float) * input.size(), cudaMemcpyHostToDevice, stream);
 
+    cudaEventCreate(&time1);
+	cudaEventCreate(&time2);
+	cudaEventCreate(&time3);
+	cudaEventCreate(&time4);
+
+    cudaEventRecord(time1, 0);		// Before FFT
     // Run FFT and copy data
     cufftExecR2C(planReal, d_input, d_output);
 
     cudaMemcpyAsync(output.data(), d_output, sizeof(complex<float>) * output.size(), cudaMemcpyDeviceToHost, stream);
 
     cudaStreamSynchronize(stream);
+
+    cudaEventRecord(time2, 0);		// After FFT and Copy
 
     if (debug == 1){
         printf("Output after Forward FFT:\n");
@@ -110,19 +126,19 @@ int main(int argc, char *argv[]) {
     }
 
     // Normalize the data
-    kernel<<<1, FFT_SIZE, 0, stream>>>(d_output, 1.f/FFT_SIZE);
+    kernel<<<blockSize, FFT_SIZE/blockSize, 0, stream>>>(d_output, 1.f/(bins-1));
 
     cudaMemcpyAsync(output.data(), d_output, sizeof(complex<float>) * output.size(), cudaMemcpyDeviceToHost, stream);
 
     cudaStreamSynchronize(stream);
 
     // Magnitude
-    for (int i = 0; i < (FFT_SIZE/2+1); i++)
+    for (int i = 0; i < (bins); i++)
     {
         data_mag_sq[i] = sqrt(output[i].real() * output[i].real() + output[i].imag() * output[i].imag());
     }
     data_mag_sq[0] /= 2;
-    data_mag_sq[(FFT_SIZE/2)-1] /= 2;
+    data_mag_sq[bins-1] /= 2;
 
     if (debug){
         printf("Values with magnitude squared:\n");
@@ -134,6 +150,8 @@ int main(int argc, char *argv[]) {
 
     vector<peak> peaks(FFT_SIZE);
     find_peaks(data_mag_sq, peaks);
+
+    cudaEventRecord(time3, 0);		// after normalization, magitude, and peaks
 
     // Reverse FFT
     cufftExecC2R(planComp, d_output, d_input);
@@ -148,12 +166,35 @@ int main(int argc, char *argv[]) {
         printf("======================\n");
     }
 
+    cudaEventRecord(time4, 0);		// after reverse FFT
+
+    cudaEventSynchronize(time1);
+	cudaEventSynchronize(time2);
+	cudaEventSynchronize(time3);
+	cudaEventSynchronize(time4);
+
+	cudaEventElapsedTime(&totalTime, time1, time4);
+	cudaEventElapsedTime(&ForFFTTime, time1, time2);
+	cudaEventElapsedTime(&ScalMagTime, time2, time3);
+	cudaEventElapsedTime(&RevFFTTime, time3, time4);
+
+    printf("Timings ------------------------------------------------------------------\n");
+    printf("Forward FFT Time                    =%7.2f ms\n", ForFFTTime);
+	printf("Scaling, Magnitude, and Peak Time   =%7.2f ms\n", ScalMagTime);
+	printf("Reverse FFT Time                    =%7.2f ms\n", RevFFTTime);
+	printf("Total time elapsed                  =%7.2f ms\n", totalTime);
+    printf("--------------------------------------------------------------------------\n");
+
     // Free used resources
     cudaFree(d_input);
     cudaFree(d_output);
 
     cufftDestroy(planReal);
     cufftDestroy(planComp);
+    cudaEventDestroy(time1);
+	cudaEventDestroy(time2);
+	cudaEventDestroy(time3);
+	cudaEventDestroy(time4);
     cudaStreamDestroy(stream);
 
     cudaDeviceReset();
